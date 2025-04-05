a12b9d0fc2584526b7d5057a61f9fff77c46dc33
saif amir <saif.amir888@gmail.com>
2025-04-05 11:49:58 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyB1app5adJQCgH4-pZDxEwD9EuTxi0-VA8",
+  authDomain: "paradiserppk.firebaseapp.com",
+  databaseURL: "https://paradiserppk-default-rtdb.firebaseio.com",
+  projectId: "paradiserppk",
+  storageBucket: "paradiserppk.firebasestorage.app",
+  messagingSenderId: "972430724994",
+  appId: "1:972430724994:web:7d0cf093d188ad23b2899b",
+  measurementId: "G-D6E5K1HDRY"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
