604357d5a40d26b00d6df908bf58f1820e39775c
saif amir <saif.amir888@gmail.com>
2025-04-05 08:57:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-export default {
+import { defineConfig } from "vite"
+import { resolve } from "path"
+
+export default defineConfig({
   root: "src",
-  base: "/", // Set the base path to the root
+  base: "./",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
+        join: resolve(__dirname, "src/join.html"),
+        staff: resolve(__dirname, "src/staff.html"),
+        gangs: resolve(__dirname, "src/gangs.html"),
+        events: resolve(__dirname, "src/events.html"),
+      },
+    },
   },
   server: {
     port: 3000,
     open: true,
   },
-}
+})
 
